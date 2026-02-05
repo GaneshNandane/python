75224d10016c44b232b20e26152cb43590dfdf0c
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-02-05 22:22:33 +0530

    Add comments and print statements in Python script
    Here i learn about the comments and the different ways  to define the comments. i learn the meaning of the comments on the context of the programming it is the way to explain the sections of the perticular program to others

---
+++ comments_Escape_Sequnce_Charactors.py
@@ -0,0 +1,59 @@
+
+# This is the single line comment
+
+# Below lines are lines that contains print statement that prints the output to the consle 
+print("Hello how are you?")
+
+# This is the multiline comment we can write their is also another way to write multipline comments 
+
+# This program is about knowing how is your mood right now 
+# We can tell that the program contains the multiple words in it 
+
+# Below is the multiline string literal that can be use in the replacement of the comments here also their are two ways to write string literals 
+
+# FIRST WAY 
+
+"""
+
+Below lines that contains print statement is also prints to the consle
+This lines contains a person that asking question to the person that what is his name 
+And what is he doing there
+
+"""
+
+# SECOND WAY
+
+''' 
+
+Below lines that contains print statement is also prints to the consle
+This lines contains a person that asking question to the person that what is his name 
+And what is he doing there
+
+'''
+
+
+
+print("Hey their what's your name? what are you doing there")
+
+# ESCAPE SEQUECE CHARACTERS
+
+# There are so many escape sequence characters that are present in the python some of them are we going to discuss
+'''
+
+  3. \n
+  1. \'
+  2. \"
+  4. \t
+  5. \b
+  6. \r
+
+'''
+print("Sanika has the knowledge of the domain so she can easily crack the interwive. \n")
+print("Sujal is the main in the group becasue he has the most experince in \'Tech\'. \n ")
+print("Ajay is the one that has good accademics in the subject of \"Mathematics\". \n")
+print("Atul can do the tasks related to the database becasue he has the knowledge about that. \t")
+print("Sanket is the new student to the college.  \bSo he don't knwo about the campus yet.")
+print("This is the list of Students. \rThey are the student in the collge")
+
+
+
