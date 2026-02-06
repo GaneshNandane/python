3d1aafe2aac981ee804211aa8abe642f89c1e04c
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-02-06 20:46:30 +0530

    Add examples of multiple Python data types
    This script demonstrates various Python data types including complex, string, boolean, None, integer, list, tuple, and dictionary. Each data type is instantiated and printed, along with its type.

---
+++ variables_and_data_types.py
@@ -0,0 +1,44 @@
+
+# complex data type example
+memory_address1 = complex(3, 5)
+print(memory_address1)
+
+# string data type example
+memory_address2 = "Python"
+print(memory_address2)
+
+# boolian data type example
+memory_address3 = True
+print(memory_address3)
+
+# None data type example
+memory_address4 = None
+print(memory_address4)
+
+# intiger data type example
+memory_address5 = 75  # <-------- memory_address5 is the variable name that can be used as the name to the alocated block of the memory
+print(memory_address5)
+
+# list data type example
+memory_address6 = [ 3, 34.4, [ 9, 53], ["Public", "Private"], 356, 302]
+print(memory_address6)
+
+# tuple data type example
+memory_address7 = (("Savrav", "Samprata"), ("Gavrav", "Gauri"))
+print(memory_address7)
+
+# dictionary data type example
+memory_address8 = {"Age": 20, "Name":"Sania", "Canvote":"Yes"}
+print(memory_address8)
+
+
+print(memory_address1 + memory_address5) # <---------- we can add the different variable to each other with some limitations that the data type must be same otherwise you get an unexpected errors
+
+print("Type of memory_address1", type(memory_address1)) # <---------- here we knowling the type of memory_address1
+print("Type of memory_address2", type(memory_address2)) # <---------- here we knowling the type of memory_address2
+print("Type of memory_address3", type(memory_address3)) # <---------- here we knowling the type of memory_address3
+print("Type of memory_address4", type(memory_address4)) # <---------- here we knowling the type of memory_address4
+print("Type of memory_address5", type(memory_address5)) # <---------- here we knowling the type of memory_address5
+print("Type of memory_address6", type(memory_address6)) # <---------- here we knowling the type of memory_address6
+print("Type of memory_address7", type(memory_address7)) # <---------- here we knowling the type of memory_address7
+print("Type of memory_address8", type(memory_address8)) # <---------- here we knowling the type of memory_address8
