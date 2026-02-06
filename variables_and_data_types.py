5d34b60b211abd5570a861cd442d8ae77f988f87
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-02-06 21:15:02 +0530

    Update variables_and_data_types.py
    here i learn about different data types in python like float, int, complex, string, boolean, none, list,  dictionary, tuple, set,

--- variables_and_data_types.py
+++ variables_and_data_types.py
@@ -7,7 +7,7 @@ print(memory_address1)
 memory_address2 = "Python"
 print(memory_address2)
 
-# boolian data type example
+# boolean data type example
 memory_address3 = True
 print(memory_address3)
 
@@ -15,8 +15,8 @@ print(memory_address3)
 memory_address4 = None
 print(memory_address4)
 
-# intiger data type example
-memory_address5 = 75  # <-------- memory_address5 is the variable name that can be used as the name to the alocated block of the memory
+# integer data type example
+memory_address5 = 75  # <-------- memory_address5 is the variable name that can be used as the name to the allocated block of the memory
 print(memory_address5)
 
 # list data type example
@@ -31,14 +31,23 @@ print(memory_address7)
 memory_address8 = {"Age": 20, "Name":"Sania", "Canvote":"Yes"}
 print(memory_address8)
 
+# set data type example
+memory_address9 = {3, 4, 5, 3, 6, 8, 3, 9, 7, 2, 2}
+print(memory_address9)
+
+# float data type example
+memory_address10 = 540.445
+print(memory_address10)
 
 print(memory_address1 + memory_address5) # <---------- we can add the different variable to each other with some limitations that the data type must be same otherwise you get an unexpected errors
 
-print("Type of memory_address1", type(memory_address1)) # <---------- here we knowling the type of memory_address1
-print("Type of memory_address2", type(memory_address2)) # <---------- here we knowling the type of memory_address2
-print("Type of memory_address3", type(memory_address3)) # <---------- here we knowling the type of memory_address3
-print("Type of memory_address4", type(memory_address4)) # <---------- here we knowling the type of memory_address4
-print("Type of memory_address5", type(memory_address5)) # <---------- here we knowling the type of memory_address5
-print("Type of memory_address6", type(memory_address6)) # <---------- here we knowling the type of memory_address6
-print("Type of memory_address7", type(memory_address7)) # <---------- here we knowling the type of memory_address7
-print("Type of memory_address8", type(memory_address8)) # <---------- here we knowling the type of memory_address8
+print("Type of memory_address1", type(memory_address1)) # <---------- here we knowing the type of memory_address1
+print("Type of memory_address2", type(memory_address2)) # <---------- here we knowing the type of memory_address2
+print("Type of memory_address3", type(memory_address3)) # <---------- here we knowing the type of memory_address3
+print("Type of memory_address4", type(memory_address4)) # <---------- here we knowing the type of memory_address4
+print("Type of memory_address5", type(memory_address5)) # <---------- here we knowing the type of memory_address5
+print("Type of memory_address6", type(memory_address6)) # <---------- here we knowing the type of memory_address6
+print("Type of memory_address7", type(memory_address7)) # <---------- here we knowing the type of memory_address7
+print("Type of memory_address8", type(memory_address8)) # <---------- here we knowing the type of memory_address8
+print("Type of memory_address8", type(memory_address9)) # <---------- here we knowing the type of memory_address9
+print("Type of memory_address8", type(memory_address10)) # <---------- here we knowing the type of memory_address10
