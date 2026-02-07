16a33925591cbcc99257933b26f56de8e86270ba
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-02-07 23:45:25 +0530

    Document pip usage and module examples in pip_and_modules.py
    Added explanations and examples for using pip and modules in Python.

---
+++ pip_and_modules.py
@@ -0,0 +1,16 @@
+# Pip is the command that can be trigered in the comamand prompt to extract the requered pakage or module in to our system to be able to use that pakage by default entire python is not get downloaded to your system it only download the bsic features after that if we want then we can install them as per our requirements
+# There are two types of module the one that is buildin in the python we just need to use it according to our use case and the others are the one External that are get installed by using Pip 
+
+# Example of external module
+import pandas 
+
+# Example of buildin module
+import math
+
+print("EEE")
+
+# use of the externaml module
+pandas.read_csv("resume.csv")
+
+# use of the buildin module
+print(math.pi)
