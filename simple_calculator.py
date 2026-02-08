93d78e944bb97c279e9c9e026cddd1185985a1fb
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-02-08 22:55:50 +0530

    Add simple calculator for basic arithmetic operations
    i learn how to Implement calculator in python using variables and print the output on to the screen

---
+++ simple_calculator.py
@@ -0,0 +1,16 @@
+
+# This is a simple calculator that takes two predefined variables
+a = 15
+b = 30
+
+# This prints the addition of two predefined variables
+print("The value of", a, "+", b, "is: ", a + b)
+
+# This prints the subtraction of two predefined variables
+print("The value of", a, "-", b, "is: ", a - b)
+
+# This prints the multiplication of two predefined variables
+print("The value of", a, "*", b, "is: ", a * b)
+
+# This prints the division of two predefined variables
+print("The value of", a, "/", b, "is: ", a / b)
