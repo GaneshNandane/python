2da448552017089f815c0de3addffec25f641241
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-03-07 00:09:54 +0530

    Create for_loop_with_else.py

---
+++ for_loop_with_else.py
@@ -0,0 +1,8 @@
+#  else statement can be used with the for loop by exiting the for loop the else statement is get executed
+
+i = 0
+for i in range (11):
+    print(i)
+    i += 1
+else:
+    print("All ten values are printed")
