f7fd124b536e6e5f8fb27a78e928869f5424f7ae
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-04-01 23:59:23 +0530

    Create classes_and_objects.py

---
+++ classes_and_objects.py
@@ -0,0 +1,12 @@
+
+class details:
+  name = "Ganesh"
+  age = 21
+
+  def description(self):
+    print(f" my name is {self.name} and i am {self.age} years old")
+obj = details()
+print(obj.name)
+print(obj.age)
+obj.description()
+
