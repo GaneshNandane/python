9c9d3222c7b9b0f36ca6960c3bab9c7e80ff921a
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-04-03 23:51:56 +0530

    Add default constructor to Person class
    Updated Person class to include a default constructor.

---
+++ default_constructors.py
@@ -0,0 +1,20 @@
+# Python mainly has two types of constructors:
+
+# 1. Default Constructor
+#    - It takes only one argument called 'self'
+#    - 'self' refers to the current object and binds the object to the class
+#    - It is automatically called when an object is created
+#    - It does not assign values unless explicitly written inside it
+
+# 2. Parameterized Constructor
+#    - It takes additional parameters along with 'self'
+#    - These parameters are used to assign values to the object at the time of creation
+#    - Each object gets its own values based on the arguments passed
+
+# Note: In this code, we are only discussing the default constructor
+
+class Person:
+    def __init__(self):
+        print("This is the default constructor")
+
+p1 = Person()
