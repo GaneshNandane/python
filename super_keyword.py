130074cf178fcdc12c4d390a7d7f133d9c4784ea
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-05-18 01:38:39 +0530

    Create super_keyword.py

---
+++ super_keyword.py
@@ -0,0 +1,29 @@
+class Game:
+  def __init__(self, game_name, player, health):
+    self.game_name = game_name
+    self.player = player 
+    self.health = health 
+    
+  def start(self):
+    print("game started !")
+    
+  def attack(self):
+      print("player attacked")
+
+class Battle_Royale(Game):
+    def __init__(self, game_name, player, health, weapon):
+        super().start() 
+        super().__init__(game_name, player, health)
+        self.weapon = weapon 
+        if self.health == 200:
+            print(f"{self.player} can play the game.")
+            print(f"current health of the player is {self.health}")
+            print(f"weapon selected is : {self.weapon}")
+            super().attack() 
+            while self.health > 179:
+                self.health -= 1
+                print(f"player health is dicreasing due to heavy weapon and safe zone {self.health} please play inside the zone.")
+        else:
+            print("player can not play the game due to the low health.")
+            
+game_instance = Battle_Royale("PUBG", "Goga_101", 200, "AWM")
