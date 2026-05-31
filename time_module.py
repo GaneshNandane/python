317ee4e37e2204f9e88fe4615c6967059b13ecd5
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-05-31 12:52:21 +0530

    Update time_module.py

--- time_module.py
+++ time_module.py
@@ -1,3 +1,7 @@
+# time module
+    # time module in python works with the time related functions for working with time, dates, delays and timestamps 
+    # this is the below example of time module
+# here we are providing real time local time and printing on to the console with some delay using sleep function 
 import time
 
 time_india = time.localtime()
@@ -8,3 +12,11 @@ time.sleep(5)
 time_india = time.localtime()
 formated_time1 = time.strftime("%H:%M:%S", time_india)
 print(formated_time1)
+
+# used to print current date and time on to the console in formated way 
+print(time.ctime())
+
+# this is the way to print year, month, and day individually on to the console 
+print(t.tm_year)
+print(t.tm_mon)
+print(t.tm_mday)
