86026e16f00fa4795298c5f0329c190187db6214
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-06-26 22:45:32 +0530

    Update accessing build in module in python.py

--- accessing build in module in python.py
+++ accessing build in module in python.py
@@ -1,3 +1,7 @@
+# here we using the user defined module that are created by me named example which has a function named example and a variable named hello 
+# here we using the defined functions and variable in this python file 
+# here we understand the software creation using user defined modules which can be used as a template to speed up the workflow in software development  
+
 from  example import example, hello
 example()
 print(hello)
