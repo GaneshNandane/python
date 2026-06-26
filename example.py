21c0d888af0813d65b197940fbf95097824d677f
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-06-26 22:38:56 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,3 +1,7 @@
+# This is a user-defined module.
+# It contains a function and a variable that can be imported and used in another Python program.
+
 def example():
-    print("Hey hello hoow are you")
-hello=("myself ganesh")
+    print("Hey, hello! How are you?")
+
+hello = "Myself Ganesh"
