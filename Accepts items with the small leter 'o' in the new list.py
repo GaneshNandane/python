ebf46dde1f623e7b62cfc7b9fd2e8d72cb57d2bb
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-06-26 22:25:29 +0530

    Update Accepts items with the small leter 'o' in the new list.py

--- Accepts items with the small leter 'o' in the new list.py
+++ Accepts items with the small leter 'o' in the new list.py
@@ -1,3 +1,5 @@
+# extracting names from the list which contains the letter "o" in the names lists using the list comprehension
+
 names=["Milo", "Searah", "Bruno", "Anastasia", "Rosa"]
 namesWith_o=[item for item in names if "o" in item]
 print(namesWith_o)
