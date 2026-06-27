954390c091cad6142ffdb0bc53df0db8bd7ee7ab
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-06-27 22:01:03 +0530

    Update and rename Accessing set items using a for loop.py to Accessing_set_items_using_a_for_loop.py

--- Accessing set items using a for loop.py
+++
@@ -1,3 +0,0 @@
-info = {"Carla", 19, False, 5.9}
-for item in info:
-    print(item)
---
+++ Accessing_set_items_using_a_for_loop.py
@@ -0,0 +1,6 @@
+# iterating all the elements from the set using for loop
+# using for loop printing all the values that are present in the set
+                                            
+info = {"Carla", 19, False, 5.9}
+for item in info:
+    print(item)
