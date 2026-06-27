eba8ab03fc180b629ba1090f1313e73e588d3bf3
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-06-27 22:04:46 +0530

    Add comments for accessing dictionary values
    Added comments explaining how to access dictionary values.

--- accessing single values in dictionaries.py
+++ accessing single values in dictionaries.py
@@ -1,3 +1,6 @@
+# accessing dictionary values using [] brackets
+# using the get() method to access the values inside a dictionary 
+
 info={'name':'karan', 'age':19, 'eligible':True}
 print(info['name'])
 print(info.get('eligible'))
