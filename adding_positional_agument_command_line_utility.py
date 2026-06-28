ba5b17ebf22cb6b21481c8b3ad3f5ee83d5c6ade
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-06-28 23:01:54 +0530

    Enhance argparse usage with comments
    Added comments to explain the usage of argparse and the positional argument.

--- adding_positional_agument_command_line_utility.py
+++ adding_positional_agument_command_line_utility.py
@@ -1,5 +1,17 @@
+# Importing the argparse module to create command-line interfaces.
+# Creating an ArgumentParser object to define and parse command-line arguments.
+
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("Positional", help = "description of positional  argument")
+
+# Adding a required positional command-line argument.
+parser.add_argument(
+    "Positional",
+    help="Description of the positional argument."
+)
+
+# Parsing the command-line arguments provided by the user.
 args = parser.parse_args()
-print(args.Positional )
+
+# Printing the value of the positional argument.
+print(args.Positional)
