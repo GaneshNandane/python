ac4e98b06e3241fbc87c2fec016ac05ce8a262f9
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-06-28 22:53:30 +0530

    Enhance argparse utility with comments
    Added comments to improve code clarity and understanding.

--- adding optional argument command line utility.py
+++ adding optional argument command line utility.py
@@ -1,5 +1,16 @@
+# Importing the argparse module to create command-line interfaces.
+# Creating an ArgumentParser object to define and parse command-line arguments.
+
 import argparse
-parcer = argparse.ArgumentParser()
-parcer.add_argument("-o", "--optional", help = "description of optional argument", default = "default_value")
-args = parcer.parse_args()
+parser = argparse.ArgumentParser()
+
+# Defining an optional command-line argument.
+parser.add_argument("-o", "--optional",
+                    help="Description of the optional argument.",
+                    default="default_value")
+
+# Parsing the command-line arguments.
+args = parser.parse_args()
+
+# Displaying the value of the optional argument.
 print(args.optional)
