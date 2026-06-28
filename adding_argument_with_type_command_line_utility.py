8f8bb66a8dc4e3b59872016375d29815d81cbc2c
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-06-28 22:59:36 +0530

    Enhance command-line utility with comments and help text
    Added comments for clarity and improved help text.

--- adding_argument_with_type_command_line_utility.py
+++ adding_argument_with_type_command_line_utility.py
@@ -1,5 +1,19 @@
+# Importing the argparse module to create command-line interfaces.
+# Creating an ArgumentParser object to define and parse command-line arguments.
+
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("-n", type = int, help = "descripton of integer argument", required=True)
-args = parser. parse_args()
+
+# Adding a required integer command-line argument.
+parser.add_argument(
+    "-n",
+    type=int,
+    help="Description of the integer argument.",
+    required=True
+)
+
+# Parsing the command-line arguments.
+args = parser.parse_args()
+
+# Printing the value of the integer argument.
 print(args.n)
