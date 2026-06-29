d79cbac0519c51a758385af568aa51e063abb3d7
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-06-29 21:42:08 +0530

    Update async io in python.py

--- async io in python.py
+++ async io in python.py
@@ -1,9 +1,17 @@
+# importing asyncio module to write and run asyncronous code it is used to handle the time bound codes that need a waiting for each function output to process it easyly without any bug 
+
 import asyncio
+
+# creating an asycronous function 
 async def my_async_function():
-    #asynchronous code here 
+    # pausing the function for one second without blocking the program   
     await asyncio.sleep(1)
     return "Hello, async world!"  
+
+# defining the main asyncronous function 
 async def main():
     result = await my_async_function() 
     print(result)
+    
+# returning the main asyncronous function using the asyncio event loop 
 asyncio.run(main())
