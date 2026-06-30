16cead0f6762197f534df16ed48833d3eadb15f5
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-06-30 22:40:46 +0530

    Update and rename average of two numbers.py to average_of_two_numbers.py

---
+++ average_of_two_numbers.py
@@ -0,0 +1,6 @@
+# here is an example of the average of two numbers provided by the user using input() function to demonstrate the concept of the input  
+
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+
+print("The average of two numbers entered by the user is the:",(a+b)/2)
