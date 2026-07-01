55eef9bab561565c64ab48a36477d98d7fcd2fa0
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-07-01 22:00:25 +0530

    Update and rename checking whether the item is present in list1.py to checking_whether_the_item_is_present_in_list1.py

--- checking whether the item is present in list1.py
+++
@@ -1,5 +0,0 @@
-colors=["Red", "Green", "Blue", "Yellow", "Green"]
-if"Yellow" in colors:
-    print("Yellow is present.")
-else:
-    print("Yellow is absent.")
---
+++ checking_whether_the_item_is_present_in_list1.py
@@ -0,0 +1,8 @@
+# checking if item exist in a list using 'in' keyword
+
+# checking if there is 'Yellow' are in a list or not 
+colors=["Red", "Green", "Blue", "Yellow", "Green"]
+if"Yellow" in colors:
+    print("Yellow is present.")
+else:
+    print("Yellow is absent.")
