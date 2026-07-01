6b5a67eceb0ed9d6870df523cfa8a11e52cd4030
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-07-01 21:45:25 +0530

    Fix comments formatting in tuple check example

--- check_for_item_in_Tuple_example1.py
+++ check_for_item_in_Tuple_example1.py
@@ -1,6 +1,6 @@
-# checking if the items exist in the tuple using in keyword 
+# checking if the items exist in the tuple using 'in' keyword 
 
-# here we are checking if there is Germany in the tuple or not 
+# here we are checking if there is 'Germany' in the tuple or not 
 country=("Spain", "Italy", "India", "England", "Germany",)
 if "Germany" in country:
     print("Germany is present")
