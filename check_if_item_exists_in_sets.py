897c0af1d2b61fb155eb7e1d7bb332c97b92772c
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-07-01 21:49:49 +0530

    Update and rename check if item exists in sets.py to check_if_item_exists_in_sets.py

---
+++ check_if_item_exists_in_sets.py
@@ -0,0 +1,8 @@
+# checking if the items exist in a set using 'in' keyword 
+
+# here we are checking if the 'Carla' exist in a set or not 
+info={"Carla", 19, False, 5.9}
+if "Carla" in info:
+    print("Carla is present.")
+else:
+    print("Carla is absent.")
