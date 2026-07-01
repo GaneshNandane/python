abf55778f179f7c68e22742fc11fa183626040d1
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-07-01 21:57:31 +0530

    Update and rename check that touple types can not be change in python.py to check_that_touple_types_can_not_be_change_in_python.py

---
+++ check_that_touple_types_can_not_be_change_in_python.py
@@ -0,0 +1,7 @@
+# checking if the tuples are mutable in python or not 
+# we know that tuples in python are immutable means that after assigning a tuple we can not change it's value 
+
+a = (23, 54, "heart")
+a[0] = "attack"
+
+# this gives us error saying 'tuple object does not support item assignment'
