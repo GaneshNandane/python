403fff8dde7634e9fdac6c1e21f5a5784665a30a
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-07-01 22:05:15 +0530

    Update and rename checking whether the item is present in the list2.py to checking_whether_the_ite_is_present_in_the_list2.py

--- checking whether the item is present in the list2.py
+++
@@ -1,5 +0,0 @@
-colors=["Red", "Green", "Blue", "Yellow", "Green"]
-if "Orange" in colors:
-    print("Orange is present.")
-else:
-    print("Orange is absent.")
---
+++ checking_whether_the_ite_is_present_in_the_list2.py
@@ -0,0 +1,8 @@
+# checking if the item exist in a list using 'in' keyword
+
+# here we are checking if 'Orange' is present in a list or not 
+colors=["Red", "Green", "Blue", "Yellow", "Green"]
+if "Orange" in colors:
+    print("Orange is present.")
+else:
+    print("Orange is absent.")
