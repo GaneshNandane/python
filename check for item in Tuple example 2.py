17bb177a76d0860dc2b470792d9c85c5a4a828d5
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-07-01 21:44:15 +0530

    Update check for item in Tuple example 2.py

--- check for item in Tuple example 2.py
+++ check for item in Tuple example 2.py
@@ -1,3 +1,6 @@
+# checking if there is item in the tuple using the 'in' keyword
+
+# here we are checking if there is 'Russia' in the tuple or not 
 country=("Spain", "Italy", "India", "England", "Germany",)
 if "Russia" in country:
     print("Russia is present")
