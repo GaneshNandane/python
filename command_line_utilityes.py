d853b0fd7df86350f52b681da5b078a37a8c32c6
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-07-03 22:15:44 +0530

    Add files via upload

---
+++ command_line_utilityes.py
@@ -0,0 +1,26 @@
+import argparse
+import requests
+
+def download_file(url, local_filename=None):
+    if local_filename is None:
+        local_filename = url.split('/')[-1]  # Use last part of URL as filename
+
+    with requests.get(url, stream=True) as r:  # Start the request
+        r.raise_for_status()  # Raise an error if the request fails
+        with open(local_filename, 'wb') as f:  # Open file for writing
+            for chunk in r.iter_content(chunk_size=8192):  # Read in chunks
+                f.write(chunk)
+    
+    return local_filename
+
+# Argument Parsing
+parser = argparse.ArgumentParser()
+parser.add_argument("url", help="URL of the file to download")
+parser.add_argument("-o", "--output", help="Name of the file", default=None)
+
+args = parser.parse_args()
+
+print("Downloading:", args.url)
+print("Saving as:", args.output if args.output else "Using default filename")
+
+download_file(args.url, args.output)
