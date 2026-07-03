99ed41c43068ed8d44ffd3d14c76bbb135dffcf4
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-07-03 22:40:09 +0530

    Add comments for clarity in comparison script
    Added comments to clarify the purpose of the code.

---
+++ comparison_variable_whether_the_given_variable_is_greater_than_or_not.py
@@ -0,0 +1,6 @@
+# using the comparison operator to check if the variable1 is grater that variable2 
+
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+
+print("The first number is greater than the second number:", a>b)
