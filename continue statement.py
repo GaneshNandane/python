1eb179ace9ee73e2836c69da6525141aa34d4179
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-07-04 23:07:04 +0530

    Update and rename continue statement.py to continue_statement.py

--- continue statement.py
+++
@@ -1,4 +0,0 @@
-for i in [2,3,4,6,8,0]:
-     if (1 % 2!=0):
-         continue
-         print(i)
---
+++ continue_statement.py
@@ -0,0 +1,7 @@
+# continue statement 
+
+# using the if condition to check if the number is a odd number or not if the number is odd then the value would be skiped completely 
+for i in [2, 3, 4, 6, 8, 0]:
+    if i % 2 != 0:
+        continue
+    print(i)
