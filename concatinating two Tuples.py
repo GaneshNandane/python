f628458eda8246e32c675a2fac7bd25f41c5ad0d
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-07-04 22:38:51 +0530

    Update and rename concatinating two Tuples.py to concatinating_two_Tuples.py

--- concatinating two Tuples.py
+++
@@ -1,4 +0,0 @@
-countries=("Pakistan", "Afghanistan", "Bangladesh", "ShriLanka")
-countries2=("Vietnam", "India", "China")
-southEastAsia=countries+countries2
-print(southEastAsia)
---
+++ concatinating_two_Tuples.py
@@ -0,0 +1,6 @@
+# Concatenating two tuples into a single new tuple 
+
+countries=("Pakistan", "Afghanistan", "Bangladesh", "ShriLanka")
+countries2=("Vietnam", "India", "China")
+asian_countries=countries+countries2
+print(asian_countries)
