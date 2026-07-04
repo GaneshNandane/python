beebecc4a4b69b9ad6603ecb2e48e2835ae7bf30
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-07-04 23:17:39 +0530

    Update and rename count in list.py to count_in_list.py

--- count in list.py
+++
@@ -1,5 +0,0 @@
-colors=["Voilet", "Green", "Indigo", "Blue", "Green"]
-print(colors.count("Green"))
-
-num=[4, 2, 5, 3, 6, 1, 2, 1, 3, 2, 8, 9, 7]
-print(num.count(2))
---
+++ count_in_list.py
@@ -0,0 +1,9 @@
+# counting an occurrence of the element inside a list 
+
+# here in this code we are checking the occurrence of the element 'Green' inside a list colors
+colors=["Voilet", "Green", "Indigo", "Blue", "Green"]
+print(colors.count("Green"))
+
+# here in this code we are checking the occurrence of the element '2' inside a list num 
+num=[4, 2, 5, 3, 6, 1, 2, 1, 3, 2, 8, 9, 7]
+print(num.count(2))
