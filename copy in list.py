13134e40cc40cf7d91b2f2db7ca08685c48f364e
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-07-04 23:12:24 +0530

    Update and rename copy in list.py to copy_in_list.py

--- copy in list.py
+++
@@ -1,5 +0,0 @@
-colors=["Voilet", "Green", "Indigo", "Blue"]
-newlist=colors.copy()
-print(colors)
-print(newlist)
-
---
+++ copy_in_list.py
@@ -0,0 +1,8 @@
+# copying a list into a new list 
+
+# here in this program we are copying a list into a new list named newlist which contains the exact contents of the original list 
+colors=["Voilet", "Green", "Indigo", "Blue"]
+newlist=colors.copy()
+print(colors)
+print(newlist)
+
