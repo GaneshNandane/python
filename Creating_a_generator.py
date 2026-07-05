3c916f91fe8119ab8680604298702c361b66741c
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-07-05 23:40:32 +0530

    Update Creating_a_generator.py

--- Creating_a_generator.py
+++ Creating_a_generator.py
@@ -1,3 +1,6 @@
+# Create a generator to produce numbers one at a time using manual next() calls
+# the user need to call it manually using the next function 
+
 def My_Generator():
     for i in range(5):
         yield i
