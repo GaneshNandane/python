5cf4b13f25d14f57539c6effd1cc655630b5bdac
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-07-05 23:28:55 +0530

    Update and rename creating an object.py to creating_an_object.py

---
+++ creating_an_object.py
@@ -0,0 +1,10 @@
+# creating a simple class greeting and creating a object of the class and calling greet attribute  
+
+class greeting:
+    greet = "Hello"
+
+# object creation
+obj1=greeting()
+
+# printing the attribute
+print(obj1.greet)
