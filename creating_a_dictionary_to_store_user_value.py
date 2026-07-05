4d5f91460bda3a8c5957ecdc6830c85f0c5ab637
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-07-05 23:02:12 +0530

    Update and rename create an empty dictionary. allow four friends to enter thier favorite language as value and use key as their name. assume that their names are unique.py to creating_a_dictionary_to_store_user_value.py

---
+++ creating_a_dictionary_to_store_user_value.py
@@ -0,0 +1,29 @@
+# creating a empty dictionary to store user defined value in it 
+d = {}
+
+# adding first value 
+name = input("Enter friends name:")
+lang = input("Enter language name:")
+# updating a value using update() adding key value pairs
+d.update({name: lang})
+
+# adding second value 
+name = input("Enter friends name:")
+lang = input("Enter language name:")
+# updating a value using update() adding key value pairs
+d.update({name: lang})
+
+# adding third value 
+name = input("Enter friends name:")
+lang = input("Enter language name:")
+# updating a value using update() adding key value pairs
+d.update({name: lang})
+
+# adding fourth value 
+name = input("Enter friends name:")
+lang = input("Enter language name:")
+# updating a value using update() adding key value pairs
+d.update({name: lang})
+
+# printing updated value 
+print(d)
