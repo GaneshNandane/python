7c729e3b3a7c672b97c8ce794615e678f11e233e
Ganesh Nandane <147237947+GaneshNandane@users.noreply.github.com>
2026-07-05 23:16:47 +0530

    Update create_a_thread.py

--- create_a_thread.py
+++ create_a_thread.py
@@ -1,5 +1,33 @@
+# creating a thread and executing them 
+
+# here threading works to do multiple tasks concurrently in the same program without threading task runs sequntially 
 import threading
-def my_func():
-    print("Hello from thread", threading.current_thread().name)
-    thread.start()
-    thread.join()
+import time
+
+# task one 
+def task1():
+    print("Task 1 started")
+    time.sleep(3)
+    print("Task 1 finished")
+
+# task two 
+def task2():
+    print("Task 2 started")
+    time.sleep(2)
+    print("Task 2 finished")
+
+# creating a thread one
+t1 = threading.Thread(target=task1)
+# creating a thread two 
+t2 = threading.Thread(target=task2)
+
+# starting both the threads simultaneously
+t1.start()
+t2.start()
+
+# ending both the threads 
+t1.join()
+t2.join()
+
+# printing that the both the tasks completed  
+print("Both tasks completed")
