cb6e55cd9145ed678ee5aa05f4cdb52a742458e6
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-06 23:28:44 +0530

    Add files via upload

---
+++ creating_a_pool_of_worker_processes.py
@@ -0,0 +1,8 @@
+from multiprocessing import Pool
+def process_task(task):
+    #  Do some work here
+    print("Task processed:", task)
+if __name__ == '__main__':
+    tasks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    with Pool (processes = 4) as p:
+        results = p.map(process_task, tasks)
