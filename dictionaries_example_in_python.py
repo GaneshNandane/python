5d39a84688833ed15caec39fa944f05d61e324f8
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-07 20:55:01 +0530

    Update and rename dictionaries example in python.py to dictionaries_example_in_python.py

---
+++ dictionaries_example_in_python.py
@@ -0,0 +1,7 @@
+# this is the simple example of a dictionary 
+
+# initializing a dictionary using '{}' brackets with the key value pairs seperated by ':'
+info={'name':'karan', 'age':19, 'eligible':True}
+
+# printing the dictionary 
+print(info)
