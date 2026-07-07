6037c0fc59d84fadc3879dc3bc612fa6b01aa59e
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-07 20:47:17 +0530

    Update and rename del method in dictionaries.py to del_method_in_dictionaries.py

---
+++ del_method_in_dictionaries.py
@@ -0,0 +1,7 @@
+# deleting a key value pair in dictionary using del keyword 
+
+info={'name':'karan', 'age':19, 'eligible':True, 'DOB':2003}
+del info['age']
+
+# prints the rest of the dictionary values with the key value pairs
+print(info)
