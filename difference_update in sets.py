80720dc40ddc6528efd391af2b28379d4c935787
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-08 21:01:15 +0530

    Update and rename difference_update in sets.py to difference_update_in_sets.py

--- difference_update in sets.py
+++
@@ -1,3 +0,0 @@
-cities={"Tokyo", "Madrid", "Berlin", "Delhi"}
-cities2={"Seoul", "Kabul", "Delhi"}
-print(cities.difference(cities2))
---
+++ difference_update_in_sets.py
@@ -0,0 +1,8 @@
+# difference in set 
+
+# declaring two sets for the operations 
+cities={"Tokyo", "Madrid", "Berlin", "Delhi"}
+cities2={"Seoul", "Kabul", "Delhi"}
+
+# performing the difference() method and printing the output without any any variable storage 
+print(cities.difference(cities2))
