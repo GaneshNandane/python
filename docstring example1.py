31af4712fe62cd538498644c6f238490515c4c45
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-09 23:38:23 +0530

    Add docstring to square function
    Added a docstring to the square function for clarity.

--- docstring example1.py
+++
@@ -1,4 +0,0 @@
-def square(n):
-    '''Takes in number n, returns the square of n'''
-    print(n**2)
-square(5)
---
+++ docstring_example1.py
@@ -0,0 +1,9 @@
+# here is a docstring for the square root of the number 
+
+# writing the docstring exactlly below the square function definition
+def square(n):
+    '''Takes in number n, returns the square of n'''
+    print(n**2)
+
+# calling the function
+square(5)
