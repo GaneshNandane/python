e86b17121b811be0dfbacfd5eaf7887be7755ae4
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-10 01:10:27 +0530

    Update and rename else with while loop.py to else_with_while_loop.py

---
+++ else_with_while_loop.py
@@ -0,0 +1,9 @@
+# else with the while loop 
+
+# here we are printing the variable untill the value of the variable gets to zero and condition get false and further execution stops the loop and else block is being executed 
+x=5
+while(x>0):
+    print(x)
+    x=x-1
+else:
+    print("counter is 0")
