2c99e573d9a3e22374bc2188b3e8aa1ec0bdea82
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-11 20:38:12 +0530

    Update and rename Example of classes and object.py to Example_of_classes_and_object.py

--- Example of classes and object.py
+++
@@ -1,6 +0,0 @@
-class Details:
-    name="Rohan"
-    age=20
-obj1=Details()
-print(obj1.name)
-print(obj1.age)
---
+++ Example_of_classes_and_object.py
@@ -0,0 +1,13 @@
+# this is a simple example of class and object 
+
+# this is a class named details which has two class variables name and age  
+class Details:
+    name="Rohan"
+    age=20
+
+# creating a class object (instance) using the class Details 
+obj1=Details()
+
+# printing the class variables through objects 
+print(obj1.name)
+print(obj1.age)
