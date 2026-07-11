b69b6020160e15c3d6dbfeef87860f370aab15cd
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-11 19:45:28 +0530

    Update and rename enumerate function with tuples.py to enumerate_function_with_tuples.py

---
+++ enumerate_function_with_tuples.py
@@ -0,0 +1,8 @@
+# iterating over a tuple using enumerate function with keeping track of index for each element 
+
+# here we are defining a tuple and iterating over tuple objects using enemerate function
+colors=('red', 'green', 'blue')
+for index,color in enumerate(colors):
+    
+    # printing each index and corresponding elements 
+    print(index,color)
