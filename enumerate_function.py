e0a7d22fe14ed0be51b13ced612a03da9e7d754f
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-11 19:53:14 +0530

    Refactor comments for clarity in enumerate_function.py

---
+++ enumerate_function.py
@@ -0,0 +1,8 @@
+# using enumerate function to print the index and corresponding elements 
+
+# here we are defining a list of fuits and using enumerate over the list 
+fruits=['apple', 'banana', 'mango']
+for index,fruit in enumerate(fruits):
+    
+    # printing index and corresponding elements 
+    print(index,fruit)
