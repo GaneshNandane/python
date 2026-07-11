c3c67bced4b30157689553790d60889269690641
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-11 20:26:23 +0530

    Add files via upload

---
+++ escape sequnce characters.py
@@ -0,0 +1,31 @@
+# Inserting single quotes in string.
+string = 'Hi! \'man'  
+print ("By using \: ", string)  
+
+# Inserting double quotes in string.
+string = "Hi!\"man"  
+print ("By using \: ", string)  
+
+# Inserting \n in string.
+string = "Hi\nman"  
+print ("By using \n: ", string)  
+
+# Inserting \t in string.
+string = "Hi!\tman"  
+print ("By using \\t: ",string)  
+
+# Inserting \r in string.
+string = "Hi!\rman"  
+print ("By using \\r: ",string)  
+
+# Inserting \b in string.
+string = "Hi!\bman"  
+print ("By using \\b: ",string)  
+
+# Inserting \ooo in string.
+string = "\543\432\789"  
+print (string)  
+
+# Inserting \xhh in string.
+string = "\x54\x34"  
+print (string)  
