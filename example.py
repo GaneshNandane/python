3cee44e0f8b4e254b42e0ca78c1f98134fd700a4
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-12 20:13:34 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,3 +1,12 @@
+# this is a simple example of the function named example() 
+
+# this function greets the people 
 def example():
     print("Hey hello hoow are you")
+
+# calling that function 
+example()
+
+# greeting the other person 
 hello=("myself ganesh")
+print(hello)
