442d4cdcc4f345f9e3aa362d86a565bd14cf2a68
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-12 20:07:09 +0530

    Add files via upload

--- example.py
+++ example.py
@@ -1,7 +1,3 @@
-# This is a user-defined module.
-# It contains a function and a variable that can be imported and used in another Python program.
-
 def example():
-    print("Hey, hello! How are you?")
-
-hello = "Myself Ganesh"
+    print("Hey hello hoow are you")
+hello=("myself ganesh")
