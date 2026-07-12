9d4108b3bfa95752440530a578646925fd6e7529
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-12 20:15:14 +0530

    Add files via upload

---
+++ example_of_loop_through_string.py
@@ -0,0 +1,8 @@
+# looping through a string
+
+# here we are defining a variable named alphabets and storing a string named 'ABCDE' into that variable
+alphabets="ABCDE"
+
+# iterating over the string and printing each character that are present inside that string 
+for i in alphabets:
+    print(i)
