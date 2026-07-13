f8ff74af9c9c78277e86737f91950a06cc809999
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-13 19:28:10 +0530

    Update and rename extend in list.py to extend_in_list.py

--- extend in list.py
+++
@@ -1,5 +0,0 @@
-# add a list to a list
-colors=["Voilet", "Indigo", "Blue"]
-rainbow=["Green", "Yellow", "Orange", "Red"]
-colors.extend(rainbow)
-print(colors)
---
+++ extend_in_list.py
@@ -0,0 +1,10 @@
+# add a list to a list using extend method 
+
+colors=["Voilet", "Indigo", "Blue"]
+rainbow=["Green", "Yellow", "Orange", "Red"]
+
+# it adds all element of the rainbow into colors 
+colors.extend(rainbow)
+
+# it prints the final extended list 
+print(colors)
