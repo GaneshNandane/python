88652bb119a6f8dffc541ccfc56ecff39c89bd9f
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-14 21:52:37 +0530

    Update and rename f-string in python.py to f-string_in_python.py

---
+++ f-string_in_python.py
@@ -0,0 +1,18 @@
+# f-strings are used to insert variables and expressions directly into a string.
+# They are easier to read and write than using the format() function.
+
+# Storing the string "Geeks" in a variable
+val = "Geeks"
+
+# Using an f-string to insert the value of 'val' into the string
+print(f"{val}for{val} is a portal for {val}.")
+
+# Another example of using f-strings
+
+# Storing a name and age in variables
+name = "Ganesh"
+age = 19
+
+# Inserting the variables directly into the string
+# Any values stored in 'name' and 'age' will automatically appear in the output
+print(f"Hello, my name is {name} and I'm {age} years old.")
