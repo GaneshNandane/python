db30a038c1a3b73de891397bb90b0cc04b715ddf
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-14 22:27:00 +0530

    Update and rename file handlingmain.py to file_handling_main.py

--- file handlingmain.py
+++
@@ -1,9 +0,0 @@
-# READING A File
-# f=open('file for manipulating files.py', 'r')
-# f.write('Hello world!')
-# f.close()
-
-# WRITING A File
-
-with open('file for manipulating files.py', 'a')as f:
-    f.write("Hey i am inside with")
---
+++ file_handling_main.py
@@ -0,0 +1,16 @@
+# WRITING A File
+
+# writing a file in the mode 'a' (append) mode so that the file content can not get overide and if the file does not exist then it is being created on the go 
+with open('file_for_manipulating_files.py', 'a')as f:
+    f.write("Hey i am inside with")
+    
+# READING A File
+# reading a file in the 'r' (reading) mode so we can only read the file content and not make changes accidently 
+f=open('file_for_manipulating_files.py', 'r')
+file = f.read()
+
+# printing file content 
+print(file)
+
+# close the file after reading 
+f.close()
