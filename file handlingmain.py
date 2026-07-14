45e7ded617606645fe78c0eea61d12022cc4d3b2
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-14 22:06:16 +0530

    Add files via upload

---
+++ file handlingmain.py
@@ -0,0 +1,9 @@
+# READING A File
+# f=open('file for manipulating files.py', 'r')
+# f.write('Hello world!')
+# f.close()
+
+# WRITING A File
+
+with open('file for manipulating files.py', 'a')as f:
+    f.write("Hey i am inside with")
