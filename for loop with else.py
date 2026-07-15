0d0b0dffe318e34fbcc4bc8b5cde6e234815e004
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-15 23:14:00 +0530

    Update and rename for loop with else.py to for_loop_with_else_example2.py

--- for loop with else.py
+++
@@ -1,5 +0,0 @@
-l = ["Ganesh", "Nandane", "Pooja", "Pratiksha", "Nirmala", "Harichandra", 2, 3, 4, 5, 6, 7, 7]
-for item in l:
-    print(item)
-else:
-    print("Done")
---
+++ for_loop_with_else_example2.py
@@ -0,0 +1,7 @@
+# printing all the elements of the list using for loop and iterating over all list elements and then printing else block imidiatlly after the printing all elements that are in the list 
+
+l = ["Ganesh", "Nandane", "Pooja", "Pratiksha", "Nirmala", "Harichandra", 2, 3, 4, 5, 6, 7, 7]
+for item in l:
+    print(item)
+else:
+    print("Done")
