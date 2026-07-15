90711c6294e77bdf4f60d6de1c90cff23a45d5a5
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-15 23:37:29 +0530

    Update and rename from keyword for importing in python example 2.py to from_keyword_for_importing_in_python_example_2.py

--- from keyword for importing in python example 2.py
+++
@@ -1,4 +0,0 @@
-from math import sqrt, pi
-result=sqrt(9)
-print(result)
-print(pi)
---
+++ from_keyword_for_importing_in_python_example_2.py
@@ -0,0 +1,13 @@
+# here is an example of importing external module contents inside this python file using from keyword
+
+# here we are imporing sqrt and pi from math library 
+from math import sqrt, pi
+
+# using the module contents inside this file 
+result=sqrt(9)
+
+# printing the result
+print(result)
+
+# printing the value of pi
+print(pi)
