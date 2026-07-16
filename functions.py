2f1d3c7bb12caf663d2096cd0410b2452dcc4515
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-16 19:22:15 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+# this is a simple example of function in this function there are two arguments fname and lname where name and last name are being provided and printed 
+
 def name (fname, lname):
     print("Hello,", fname, lname)
 name("Sam", "Wilson")
