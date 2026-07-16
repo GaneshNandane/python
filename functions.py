3f5d3c736b507189c3072edb9bec2237a5d646b1
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-16 19:19:48 +0530

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,15 +1,3 @@
-import math
-a = int(input("Enter one number:"))
-b = int(input("Enter another number:"))
-
-# here we are calculating arithmetic mean in this block of code 
-def calculateAmean(a, b):
-  mean = (a + b)/2
-  print("Aritmetic mean:",mean)
-calculateAmean(a, b)
-
-# here we are calculating geometric mean in this block of code
-def calculateGmean(a, b):
-  mean = math.sqrt(a * b)
-  print("Geometric mean:",mean)
-calculateGmean(a, b)
+def name (fname, lname):
+    print("Hello,", fname, lname)
+name("Sam", "Wilson")
