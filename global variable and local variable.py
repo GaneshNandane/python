e8b08923db998c9c8fb5179ac20ac98a5abf6ab9
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-17 22:07:28 +0530

    Rename global variable and local variable.py to global_variable_and_local_variable.py

--- global variable and local variable.py
+++
@@ -1,7 +0,0 @@
-x=10 #Global Variable
-def my_function():
-    y=5 #local variable
-    print(y)
-my_function()
-print(x)
-# print(y) #this will cause an error because y is a local variable and is not accessible outside of the function##
