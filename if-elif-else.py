6ddfc5f2f1878cd76fa917b3ab4b9bf7230307e8
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-18 23:15:19 +0530

    Update if-elif-else.py

--- if-elif-else.py
+++ if-elif-else.py
@@ -1,7 +1,13 @@
-num=0
+# here in this program we are checking if the number provided by the user is negative zero or positive 
+
+num = int(input("Enter any number: "))
+
+# condition for negative  
 if(num<0):
     print("Number is negative.")
+# condition for zero  
 elif(num==0):
     print("Number is zero.")
+# condition for positive  
 else:
     print("Number is positive.")
