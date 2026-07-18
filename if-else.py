ab7871a5aee83208adaa281ebe9f404348df5310
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-18 23:21:08 +0530

    Update if-else.py

--- if-else.py
+++ if-else.py
@@ -1,5 +1,11 @@
+# here in this program we are checkig if we can buy some apples form the store with comparing price of apples with our budget 
+
+# apple price 
 applePrice=210
+# our budget 
 budget=200
+
+# checking the condition if the appleprice is lower than our budget then and then only we can buy some apples 
 if(applePrice<=budget):
     print("Alexa, add 1 kg Apples to the Cart.")
 else:
