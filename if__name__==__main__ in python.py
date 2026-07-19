348892448489d2c046c89d12532281174afa3177
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-19 19:35:09 +0530

    Update and rename if__name__==__main__ in python.py to if__name__==__main__in_python.py

--- if__name__==__main__ in python.py
+++
@@ -1,5 +0,0 @@
-def main():
-    #code to be run when the script is run directly
-    print("Running script directly")
-    if __name__=="__main__":
-        main()
---
+++ if__name__==__main__in_python.py
@@ -0,0 +1,9 @@
+# defining a main() function 
+
+def main():
+    #code to be run when the script is run directly
+    print("Running script directly")
+
+# checking wheter the file is being run directlly or not  
+if __name__=="__main__":
+    main()
