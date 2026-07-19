4c47ea927cf0eeaa57ed317b284e9b3158bb4652
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-19 19:11:09 +0530

    Update and rename if else in one lineexample 1.py to if_else_in_one_line_example_1.py

---
+++ if_else_in_one_line_example_1.py
@@ -0,0 +1,6 @@
+# example of if else statement in one line 
+# here we are defining if else statements in one line that functions same as normal if else statement    
+
+a=2
+b=330
+print("A")if a>b else print("B")
