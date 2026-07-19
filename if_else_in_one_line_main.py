512316009d0055bba7fc4f4d10d7c68d156c6e61
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-19 19:19:18 +0530

    Update and rename if else in one linemain.py to if_else_in_one_line_main.py

---
+++ if_else_in_one_line_main.py
@@ -0,0 +1,13 @@
+# example of if-elif-else in one line 
+
+# assigning a variable  
+a=330000
+b=3303
+
+# printing 'A' if a > b else printing '=' if a == b else printing 'B'
+print("A")if a>b else print("=")if a==b else print("B")
+
+# printing value of c on the basis of a and b
+# printing c as 9 if the a > b else printing value of c as 0 
+c=9 if a>b else 0
+print(c)
