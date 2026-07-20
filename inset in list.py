2a52a235e606e215d5365ea36b7b839e7888f7e8
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-20 22:33:46 +0530

    Add files via upload

---
+++ inset in list.py
@@ -0,0 +1,6 @@
+colors=["Voilet", "Indigo", "Blue"]
+    #      [0]       [1]      [2]
+colors.insert(1, "Green") #inserts item at index 1
+#updated list:colors=["Voilet", "Green", "Indigo", "Blue"]
+# indexes               [0]       [1]      [2]       [3]
+print(colors)
