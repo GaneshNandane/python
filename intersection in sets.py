b8039c17b41b5c9673a408252669d10bee821284
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-21 23:27:21 +0530

    Update and rename intersection in sets.py to intersection_in_sets.py

--- intersection in sets.py
+++
@@ -1,4 +0,0 @@
-cities={"Tokyo", "Madrid", "Berlin", "Delhi"}
-cities2={"Tokyo", "Seoul", "Kabul", "Madrid"}
-cities3=cities.intersection(cities2)
-print(cities3)
---
+++ intersection_in_sets.py
@@ -0,0 +1,11 @@
+# intersection() method return the same elements that are present in both sets  
+
+# defining two sets 
+cities={"Tokyo", "Madrid", "Berlin", "Delhi"}
+cities2={"Tokyo", "Seoul", "Kabul", "Madrid"}
+
+# applying intersection method 
+cities3=cities.intersection(cities2)
+
+# printing the new set after applying intersection method 
+print(cities3)
