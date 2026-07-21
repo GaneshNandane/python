4ef0cbce1d003515f3f7e344632bb66a6014eb29
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-21 23:39:49 +0530

    Update and rename isdisjoint method in sets.py to isdisjoint_method_in_sets.py

--- isdisjoint method in sets.py
+++
@@ -1,3 +0,0 @@
-cities={"Tokyo", "Madrid", "Berlin", "Delhi"}
-cities2={"Tokyo", "Seoul", "Kabul", "Madrid"}
-print(cities.isdisjoint(cities2))
---
+++ isdisjoint_method_in_sets.py
@@ -0,0 +1,8 @@
+# The isdisjoint() method checks whether two sets have any common elements. It returns True if the sets have no common elements. It returns False if the sets have one or more common elements.
+
+# defining two sets
+cities={"Tokyo", "Madrid", "Berlin", "Delhi"}
+cities2={"Tokyo", "Seoul", "Kabul", "Madrid"}
+
+# printing if the sets are disjoint or not 
+print(cities.isdisjoint(cities2))
