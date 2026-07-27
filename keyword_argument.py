44593cc74faecc55135c248dca682446613439b3
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-27 16:43:02 +0530

    Update and rename keyword argument.py to keyword_argument.py

---
+++ keyword_argument.py
@@ -0,0 +1,9 @@
+# example of keyword arguments function
+def name(fname, mname, lname):
+
+    # Printing the full name
+    print("Hello,", fname, mname, lname)
+
+# Calling the function using keyword arguments.
+# Since keyword arguments are used, the order of the arguments does not matter.
+name(mname="Peter", lname="Wesker", fname="Jade")
