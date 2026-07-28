4823b486ce80fb743081f85c14c0b08c4e58113b
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-28 20:42:40 +0530

    Update and rename lambda function with multiple arguments.py to lambda_function_with_multiple_arguments.py

--- lambda function with multiple arguments.py
+++
@@ -1,6 +0,0 @@
-#function to calculate the product of two mumbers
-def multiply (x, y):
-    return x*y
-
-#lambda function to calculate the product of two numbers
-lambda x, y: x*y
---
+++ lambda_function_with_multiple_arguments.py
@@ -0,0 +1,12 @@
+# normal function to calculate the product of two mumbers
+def multiply (x, y):
+    return x*y
+
+# calculating the product of two numbers using normal function
+print(multiply(4, 5))
+
+# lambda (anonymous) function to calculate the product of two numbers
+multiply = lambda x, y: x*y
+
+# calculating the product of two numbers using lambda (anonymous) function 
+print(multiply(5, 6))
