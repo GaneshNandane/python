1a28bc06dd64a94b0ca609fb59c0e86a726ddefe
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-28 22:24:54 +0530

    Add files via upload

---
+++ letter formating using escape sequence characters.py
@@ -0,0 +1,7 @@
+# Before formating the letter
+letter = "Dear Ganesh, this course is nice. Thanks!"
+print(letter)
+
+# After fromting the letter
+letter = "Dear Ganesh, \n\t This course is nice. \n Thanks!"
+print(letter)
