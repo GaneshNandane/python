37975ba5d466c9590106975738603345e75c52ed
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-28 20:50:18 +0530

    Update and rename lambda function with statements.py to lambda_function_with_statements.py

--- lambda_function_with_statements.py
+++ lambda_function_with_statements.py
@@ -1,3 +1,6 @@
 #lambda function to calculate the product of two numbers
 #with additonal print statement
-lambda x,y:print(f'{x}*{y}={x*y}')
+
+multiply = lambda x,y:print(f'{x} * {y} = {x*y}')
+
+multiply(3, 5)
