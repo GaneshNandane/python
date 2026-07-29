f60019e6bbb4fe8a458f3d3eb46c5e6278fa129e
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-29 22:56:12 +0530

    Add files via upload

---
+++ list and tuple and string iterable.py
@@ -0,0 +1,14 @@
+# for list iterabel 
+l = [2, 4, 34, "sgdd", 243,]
+for i in l:
+    print(i)
+
+# for tuple iterable 
+t = (3, 434, 5, "#445", "dgh", 34)  
+for i in t:
+    print(i)  
+    
+# for string iterable  
+s = "Ganesh Harichandra Nandane"
+for i in s:
+    print(i)
