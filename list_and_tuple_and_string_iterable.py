f5dc97f1a65ba4f559f8cce56009685c4b222cb0
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-29 22:58:07 +0530

    Update and rename list and tuple and string iterable.py to list_and_tuple_and_string_iterable.py

--- list_and_tuple_and_string_iterable.py
+++ list_and_tuple_and_string_iterable.py
@@ -1,13 +1,16 @@
+# iterating over a list elements using for loop 
 # for list iterabel 
 l = [2, 4, 34, "sgdd", 243,]
 for i in l:
     print(i)
 
+# iterating over a tuple elements using for loop 
 # for tuple iterable 
 t = (3, 434, 5, "#445", "dgh", 34)  
 for i in t:
     print(i)  
     
+# iterating over a string elements using for loop 
 # for string iterable  
 s = "Ganesh Harichandra Nandane"
 for i in s:
