485206508fceb016ccf84381f40ccf0b2bfd4d1e
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-29 23:15:30 +0530

    Update and rename listIndex.py to list_index.py

--- listIndex.py
+++
@@ -1,3 +0,0 @@
-colors=["Red", "Green", "Blue", "Yellow", "Green"]
-        # [0]    [1]      [2]     [3]       [4]
-print(colors)
---
+++ list_index.py
@@ -0,0 +1,8 @@
+# this is the definition of how list elements gets its indexing 
+# the indexing starts from zero every time 
+
+colors=["Red", "Green", "Blue", "Yellow", "Green"]
+        # [0]    [1]      [2]     [3]       [4]
+
+# printing the list 
+print(colors)
