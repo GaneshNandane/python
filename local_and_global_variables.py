8cbf49880ca5a7d773f28ca58d19faa4f606ef73
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-30 23:46:23 +0530

    Update and rename local and global variables.py to local_and_global_variables.py

--- local_and_global_variables.py
+++ local_and_global_variables.py
@@ -2,7 +2,9 @@ x=10 #global variable
 def my_function():
     global x
     x=5 # this will change the vlaue of the global variable x
+    # this is the local variable this is limited to this function we can only use this variable within this function 
     y=5 #local variable 
+    print(y)
 my_function()
 print(x) #prints 5
 # print(y) #this will cause and error because y is a local variable and is not accessible outside of the function
