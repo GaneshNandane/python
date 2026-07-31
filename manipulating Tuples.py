752f846f05802f817e2ae0195f4cf7b0f3ccafaa
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-31 20:56:16 +0530

    Add files via upload

---
+++ manipulating Tuples.py
@@ -0,0 +1,9 @@
+countries=("Spain", "Italy", "India", "England", "Germany")
+temp=list(countries)
+temp.append("Russia")   #add item
+countries=tuple(temp)
+print(countries)
+temp.pop(3)     #remove item
+temp[2]="Finland"   #change item
+countries=tuple(temp)
+print(countries)
