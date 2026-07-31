bbe9e58e10d2c99a2eae69eacd7cb0e74789eaf8
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-07-31 21:24:43 +0530

    Update and rename map function.py to map_function.py

--- map_function.py
+++ map_function.py
@@ -2,7 +2,7 @@
 numbers=[1,2,3,4,5]
 
 #Double each number using the map function
-doubled=map(lambda x: x * 2, numbers)
+doubled=list(map(lambda x: x * 2, numbers))
 
 #print the doubled numbers
-print(list(doubled))
+print(doubled)
