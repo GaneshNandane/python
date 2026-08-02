c9fab3804daf8788145b730f2c4cf75acf247806
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-02 19:31:58 +0530

    Update and rename multiplication table using given input.py to multiplication_table_using_given_input.py

--- multiplication_table_using_given_input.py
+++ multiplication_table_using_given_input.py
@@ -1,3 +1,5 @@
+# this is the simple table printing on the basis of user value 
+
 n = int(input("Enter the number which you want to calculate table of it: "))
 for i in range(1, 11):
     print(f"{n} X {i} = {n*i}")
