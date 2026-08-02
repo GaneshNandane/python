fd15148066ce498c9bb391f9c9df8dfabe8625ca
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-02 20:12:46 +0530

    Add files via upload

---
+++ name greeting with a name that starts with s.py
@@ -0,0 +1,7 @@
+# code for the greeting of a person which name startswith s
+l = ["Soham", "Harry", "Ganesh", "Pooja"]
+for name in l:
+    if(name.startswith("S")):
+         print(f"hello {name}")
+        
+   
