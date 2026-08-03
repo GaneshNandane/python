40f28abc5d7f1f616f16d8425b2bbfe9b90b8626
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-03 21:11:48 +0530

    Add files via upload

---
+++ opening a file.py
@@ -0,0 +1,11 @@
+# Open the file in read mode
+f = open("poem.py", "r")
+
+# Read the entire contents of the file
+content = f.read()
+
+# Display the file contents
+print(content)
+
+# Close the file
+f.close()
