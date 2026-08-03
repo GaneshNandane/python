e9bb48d4103a147a9ac99575c8a77bf57736a582
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-03 20:26:32 +0530

    Add files via upload

---
+++ object-oriented programming.py
@@ -0,0 +1,22 @@
+def hello():
+    print("Hello")
+hello()
+# Sales1 = 6000
+# profit1 = 2000
+# ad1 = 1000
+# #rajeev.sales
+
+# Sales2 = 6000
+# profit2 = 2000
+# ad2 = 1000
+# #vikrant.sales
+
+# Sales3 = 6000
+# profit3 = 2000
+# ad3 = 1000
+
+# RailwayForm --> Class [blueprint]
+# harry --> harry ki info wala form --> object [entity]
+# tom --> tom ki info wala form --> object [entity]
+# shubham --> shubham ki info wala form --> object [entity]
+# # shubham.changeName("Shubhi")
