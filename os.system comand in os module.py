e43d6305c83a66370b798e344fb48abb0dab8756
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-04 20:54:54 +0530

    Update and rename os.system comand in os module.py to os.system_comand_in_os_module.py

--- os.system comand in os module.py
+++
@@ -1,5 +0,0 @@
-import os
-
-#Run the "ls" command and print the output.
-output=os.system("ls")
-print(output)       #output: ['Myfile.txt', 'otherfile.txt']
---
+++ os.system_comand_in_os_module.py
@@ -0,0 +1,6 @@
+# printing all the files present in the current directory
+import os
+
+#Run the "dir" command and print the output.
+output=os.system("dir")
+print(output)       #output: ['Myfile.txt', 'otherfile.txt']
