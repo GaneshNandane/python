70316ce8c3b0de49a806515478f006377e588483
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-04 23:35:16 +0530

    Update and rename pass keyword in python.py to pass_keyword_in_python.py

--- pass keyword in python.py
+++
@@ -1,7 +0,0 @@
-for i in range(499):
-    pass
-    
-i=0
-while(i<4):
-    print(i)
-    i += 1
---
+++ pass_keyword_in_python.py
@@ -0,0 +1,6 @@
+# the pass is a statement placeholder where we can later write the statement and complete the program by adding it we a saying that this section of the code we are going to add later 
+# it still runs the program as it is and do not perform this section of the program it mailny used for debuging purposes 
+
+for i in range(499):
+    pass
+    
