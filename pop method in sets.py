e71534ba9f6d341079a6091fa7dab03f06de3deb
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-05 22:37:06 +0530

    Update and rename pop method in sets.py to pop_method_in_sets.py

--- pop method in sets.py
+++
@@ -1,4 +0,0 @@
-cities={"Tokyo", "Madrid", "Berlin", "Delhi"}
-item=cities.pop()
-print(cities)
-print(item)
---
+++ pop_method_in_sets.py
@@ -0,0 +1,14 @@
+# The pop() method is used to remove and return
+# an arbitrary element from a set.
+
+# Define a set
+cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+
+# Remove and store an arbitrary element from the set
+item = cities.pop()
+
+# Print the set after removing an element
+print(cities)
+
+# Print the element that was removed from the set
+print(item)
