eb5c2209414f5a3651a4a0d47b2948881e603a97
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-05 22:50:17 +0530

    Update and rename popitem method in dictionaries.py to popitem_method_in_dictionaries.py

--- popitem method in dictionaries.py
+++
@@ -1,4 +0,0 @@
-info={'name':'karan', 'age':19, 'eligible':True, 'DOB':2003}
-info.popitem()
-print(info)
-#We can also use del keyword to remove
---
+++ popitem_method_in_dictionaries.py
@@ -0,0 +1,11 @@
+# popitem removes the last key value pair of the dictionary 
+
+# defining a dictionary 
+info={'name':'karan', 'age':19, 'eligible':True, 'DOB':2003}
+
+# removeing the last key value pair of dictinary 
+info.popitem()
+
+# printing the dictionary after removeing the last key vlaue pair of the dictionary
+print(info)
+#We can also use del keyword to remove
