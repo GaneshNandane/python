ecbaeefd5417010990a6d706c73593d897a8819b
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-05 22:29:33 +0530

    Update and rename pop method in dictionaries.py to pop_method_in_dictionaries.py

---
+++ pop_method_in_dictionaries.py
@@ -0,0 +1,10 @@
+# pop method is used to delete a certain key value pair from the dictionary we need to provide the key in the place of argument 
+
+# defining a dictionary 
+info={'name':'karan', 'age':19, 'eligible':True}
+
+# deleting the key value pair
+info.pop('eligible')
+
+# printing the dictionary after key value deletion
+print(info)
