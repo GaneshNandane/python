d423025fc737cb52bdbac5ed1d693d7528752584
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-06 22:27:00 +0530

    Update and rename print alternate values in Tuples.py to print_alternate_values_in_tuples.py

--- print_alternate_values_in_tuples.py
+++ print_alternate_values_in_tuples.py
@@ -1,3 +1,6 @@
+# printing the alternate values of tuple 
+
+# defining a tuple
 animals=("cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow",)
 print(animals[::2]) #using positive indexes
 print(animals[-8:-1:2]) #using negative indexes
