d4dbf16e1624a17ac37601790d9759da8d8b8214
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-06 20:25:37 +0530

    Update and rename positive indexing in Tuples.py to positive_indexing_in_tuples.py

--- positive indexing in Tuples.py
+++
@@ -1,5 +0,0 @@
-country=("Spain", "Italy", "India",)
-#         [0]      [1]      [2]
-print(country[0])
-print(country[1])
-print(country[2])
---
+++ positive_indexing_in_tuples.py
@@ -0,0 +1,14 @@
+# this is the simple example of positive indexing in tuple
+
+# defining a tuple
+country=("Spain", "Italy", "India",)
+#         [0]      [1]      [2]
+
+# accessing the element at index 0
+print(country[0])
+
+# accessing the element at index 1
+print(country[1])
+
+# accessing the element at index 2
+print(country[2])
