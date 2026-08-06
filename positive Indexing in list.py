4dc23a7d7fe7cac60dca208eae0a142be85c2da8
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-06 20:21:26 +0530

    Update and rename positive Indexing in list.py to positive_Indexing_in_list.py

--- positive Indexing in list.py
+++
@@ -1,5 +0,0 @@
-colors=["Red", "Green", "Blue", "Yellow", "Green"]
-        # [0]    [1]      [2]     [3]       [4]  
-print(colors[2])
-print(colors[4])
-print(colors[0])
---
+++ positive_Indexing_in_list.py
@@ -0,0 +1,14 @@
+# this is the simple example of positive indexing in lists 
+
+# defining a list
+colors=["Red", "Green", "Blue", "Yellow", "Green"]
+        # [0]    [1]      [2]     [3]       [4]  
+
+# accessing the element at index 2 
+print(colors[2])
+
+# accessing the element at index 4
+print(colors[4])
+
+# accessing the element at index 0
+print(colors[0])
