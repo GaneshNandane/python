85ff251615964c0c2ecd60f791faa89646c7e52a
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-07 21:53:21 +0530

    Update and rename printing all elements from a given index till the end in list.py to printing_all_elements_from_a_given_index_till_the_end_in_list.py

--- printing_all_elements_from_a_given_index_till_the_end_in_list.py
+++ printing_all_elements_from_a_given_index_till_the_end_in_list.py
@@ -1,3 +1,6 @@
+# printing all elements in list using positive and negative indexing 
+
+# defining a list
 animals=["Cat", "Dog", "Bat", "Mouse", "Pig", "Horse", "Donkey", "Goat", "Cow"]
 print(animals[4:])  #using positive indexes
 print(animals[-4:]) #using negative indexes
