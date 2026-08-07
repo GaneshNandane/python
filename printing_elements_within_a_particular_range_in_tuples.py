5f41c04e05ced8ae9bad1e4fbbff6bca1aee6e7f
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-07 22:05:36 +0530

    Update and rename printing all elements within a particular range in Tuples.py to printing_elements_within_a_particular_range_in_tuples.py

--- printing_elements_within_a_particular_range_in_tuples.py
+++ printing_elements_within_a_particular_range_in_tuples.py
@@ -1,3 +1,6 @@
+# printing elements with a perticular range
+
+# defining a tuple
 animals=("cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow",)
 print(animals[3:7]) #using positive indexes
 print(animals[-7:-2])   #using negative indexes
