c6edf649e1ce7f40534bf8a18cb2e14d9182c924
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-07 21:58:42 +0530

    Update and rename printing all elements from start to a given index in list.py to printing_all_elements_from_start_to_a_given_index_in_list.py

--- printing_all_elements_from_start_to_a_given_index_in_list.py
+++ printing_all_elements_from_start_to_a_given_index_in_list.py
@@ -1,3 +1,6 @@
+# printing all elements from start to a index
+
+# defining a list
 animals=["Cat", "Dog", "Bat", "Mouse", "Pig", "Horse", "Donkey", "Goat", "Cow"]
 print(animals[:6])  #using positive indexes
 print(animals[:-3]) #using negative indexes
