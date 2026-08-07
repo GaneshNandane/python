744da168f52b19d35434629da5664454a10c64e3
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-07 22:02:13 +0530

    Update and rename printing all elements from start to a given index in Tuples.py to printing_all_elements_from_start_to_a_given_index_in_tuples.py

--- printing_all_elements_from_start_to_a_given_index_in_tuples.py
+++ printing_all_elements_from_start_to_a_given_index_in_tuples.py
@@ -1,3 +1,6 @@
+# printing all elements from start to a given index
+
+# defining a tuple
 animals=("cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow",)
 print(animals[:6])  #using positive indexes
 print(animals[:-3]) #using negative indexes
