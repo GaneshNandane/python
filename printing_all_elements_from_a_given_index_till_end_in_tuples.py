7e371e9dbea007bfb166321e42473cd6e2a69174
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-07 21:49:27 +0530

    Update and rename printing all elements from a given index till end in Tuples.py to printing_all_elements_from_a_given_index_till_end_in_tuples.py

--- printing_all_elements_from_a_given_index_till_end_in_tuples.py
+++ printing_all_elements_from_a_given_index_till_end_in_tuples.py
@@ -1,3 +1,6 @@
+# printing all elements of the tuple using positive as well as negative indexing
+
+# defining a tuple 
 animals=("cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow",)
 print(animals[4:])  #using positive indexes
 print(animals[-4:]) #using negative indexes
