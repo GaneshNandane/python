fdfe02bd55006caf4e01cbe4a5c3209068d970cd
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-08 22:43:08 +0530

    Update and rename printing every 3rd consecutive value within a given range.py to printing_every_3rd_consecutive_value_within_a_given_range.py

--- printing every 3rd consecutive value within a given range.py
+++
@@ -1,2 +0,0 @@
-animals=["Cat", "Dog", "Bat", "Mouse", "Pig", "Harse", "Donkey", "Goat", "Cow"]
-print(animals[1:8:3])
---
+++ printing_every_3rd_consecutive_value_within_a_given_range.py
@@ -0,0 +1,5 @@
+# printing values with a step size of 3
+
+# defining a list
+animals=["Cat", "Dog", "Bat", "Mouse", "Pig", "Harse", "Donkey", "Goat", "Cow"]
+print(animals[1:8:3]) #using slicing with a step size of 3
