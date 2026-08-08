51f8f5281888251e413c77a168545ed1a6a51bff
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-08 23:14:19 +0530

    Update and rename Program to count the number of zeros in the following tuple.py to program_to_count_the_number_of_zeros_in_the_following_tuple.py

---
+++ program_to_count_the_number_of_zeros_in_the_following_tuple.py
@@ -0,0 +1,10 @@
+# printing the number of times zero present in the list
+
+# defining a list
+a = [1, 1, 2, 0, 4, 6, 12, 3, 0]
+
+# counting zero how many times occure in the list
+n = a.count(0)
+
+# printing how many times zeros are present in the list
+print(n)
