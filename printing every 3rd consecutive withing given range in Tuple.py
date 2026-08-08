ef3057ae6127b1a332bc158cca22b7abe86ff3f5
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-08 22:45:42 +0530

    Update and rename printing every 3rd consecutive withing given range in Tuple.py to printing_every_3rd_consecutive_withing_given_range_in_tuple.py

--- printing every 3rd consecutive withing given range in Tuple.py
+++
@@ -1,2 +0,0 @@
-animals=("cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow")
-print(animals[1:8:3])
---
+++ printing_every_3rd_consecutive_withing_given_range_in_tuple.py
@@ -0,0 +1,5 @@
+# printing values with a step size of 3
+
+# defining a tuple
+animals=("cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow")
+print(animals[1:8:3]) #using slicing with a step size of 3
