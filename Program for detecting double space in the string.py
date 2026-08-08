8b32aa2a4c02aba7a8494f460bc018faafdceded
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-08 22:59:37 +0530

    Add files via upload

---
+++ Program for detecting double space in the string.py
@@ -0,0 +1,9 @@
+# to find double space in the fillowing string
+name = "Hi how are you my i help you"
+print(name.find("  "))
+
+# after adding double space it gives the index of the double space
+name = "Hi how are  you my i help you"
+print(name.find("  "))
+
+# Note: it finds first occurence of the match in the parent string
