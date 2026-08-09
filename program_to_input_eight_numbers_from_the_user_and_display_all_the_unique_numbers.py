518ad0b8c747da0709de99bbe33e8e173c004e70
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-09 17:17:29 +0530

    Refactor input logic and add comments for clarity
    Refactored code to improve clarity and added comments.

--- program_to_input_eight_numbers_from_the_user_and_display_all_the_unique_numbers.py
+++ program_to_input_eight_numbers_from_the_user_and_display_all_the_unique_numbers.py
@@ -1,5 +1,11 @@
+# simple program to store the numbers to the set provided by the user and disply that set  
+
+# defining a set 
 s = set()
+
+# taking input from the user 
 n = input("Enter the number:")
+# adding the inputed value to the set
 s.add(int(n))
 n = input("Enter the number:")
 s.add(int(n))
@@ -15,4 +21,6 @@ n = input("Enter the number:")
 s.add(int(n))
 n = input("Enter the number:")
 s.add(int(n))
+
+# printing the final set
 print(s)
