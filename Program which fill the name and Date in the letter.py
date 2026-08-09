f435e9a71ac732af89a501f1765024cdbdf1f4f9
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-09 17:38:25 +0530

    Add files via upload

---
+++ Program which fill the name and Date in the letter.py
@@ -0,0 +1,11 @@
+# Before filling the name and Date in the letter
+letter = """Dear <|Name|>,
+\tyou are selected!
+<|Date|>\n"""
+print(letter)
+
+# After filling the name of the person in place of the <|Name|>
+print(letter.replace("<|Name|>", "Ganesh"))
+
+# After filling the name and date in place of the <|Name|> and <|Date|>
+print(letter.replace("<|Name|>", "Ganesh").replace("<|Date|>", "24 september 2024"))
