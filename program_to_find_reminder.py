61675d3a207bf12f705ea2023cd1b35dcc33a199
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-10 22:14:35 +0530

    Update program_to_find_reminder.py

--- program_to_find_reminder.py
+++ program_to_find_reminder.py
@@ -1,3 +1,10 @@
-a = 56333333334343434 
+# this is the simple program to find reminder of the two numbers
+
+# defining a variable
+
+a = 56333333334343434
 b = 3
+
+# printing the reminder
+
 print("the remainder is", a%b)
