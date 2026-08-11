22bb5a4ccda2a74d78c8ace685de1512528de127
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-11 22:23:36 +0530

    Update and rename range function with for loop and jumping statement argument.py to range_function_with_fo_loop_and_jumping_statement_argument.py

---
+++ range_function_with_fo_loop_and_jumping_statement_argument.py
@@ -0,0 +1,5 @@
+# this is the simple example of the range function with for loop with arguments 
+
+# here we are going to print every 4th value on to the console till 100 using range function
+for i in range(0, 100, 4):
+    print(i)
