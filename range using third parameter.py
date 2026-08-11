7f8182063b5cd729486194094ff536e01133e4b6
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-11 23:06:28 +0530

    Update and rename range using third parameter.py to range_using_third_parameter.py

--- range using third parameter.py
+++
@@ -1,2 +0,0 @@
-for k in range(1, 12, 3):
-    print(k)
---
+++ range_using_third_parameter.py
@@ -0,0 +1,5 @@
+# this is the simple example of the range function with the thired argument
+
+# this program prints the values from 1 to 12 with the jump index of 3 
+for k in range(1, 12, 3):
+    print(k)
