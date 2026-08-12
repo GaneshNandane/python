b105edaa3296b9abf91fd958b8e42141a5e1b6dc
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-12 22:28:14 +0530

    Update re.search_in_regular_expressions.py.py

--- re.search_in_regular_expressions.py.py
+++ re.search_in_regular_expressions.py.py
@@ -1,9 +1,17 @@
+# program to find the pattern on the basis of if the pattern is avaiable then printing match found else match not found 
+
 import re
-#Define a regular expression pattern 
-pattern = r"expression"
-#Match the pattern against a string text = "Hello, world!"
+
+# Define a regular expression pattern
+pattern = r"world"
+
+# Define the text in which we want to search for the pattern
 text = "Hello world!"
+
+# Search for the pattern in the text
 match = re.search(pattern, text)
+
+# Check whether a match was found
 if match:
     print("Match found!")
 else:
