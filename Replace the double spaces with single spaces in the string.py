00a98d52de6e3524333f897dbf485bc486a108b2
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-13 20:56:09 +0530

    Add files via upload

---
+++ Replace the double spaces with single spaces in the string.py
@@ -0,0 +1,9 @@
+# program for the relacement of the double space with single space
+name = "Hi how are you my  i help you\n"
+
+# Before replacing the string with double space with single space 
+print(name)
+
+# after replacing the string with double space with single space 
+print(name.replace("  "," "))
+
