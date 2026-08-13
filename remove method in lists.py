5c06640a9916bd9bd2b42253f4ddc36016a4e705
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-13 20:53:05 +0530

    Update and rename remove method in lists.py to remove_method_in_lists.py

--- remove method in lists.py
+++
@@ -1,3 +0,0 @@
-my_list = [10, 20, 30, 40]
-my_list.remove(30)
-print(my_list)
---
+++ remove_method_in_lists.py
@@ -0,0 +1,10 @@
+# simple program to remove the perticular element from the list
+
+# defining a list
+my_list = [10, 20, 30, 40]
+
+# removing element 30 from the list
+my_list.remove(30)
+
+# printing the final list after deleting the element 30 from the list
+print(my_list)
