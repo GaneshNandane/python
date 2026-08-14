c0bad8cb610064370a06aea7d3829768278271d5
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-14 22:49:05 +0530

    Update and rename reverse in list.py to reverse_in_list.py

--- reverse in list.py
+++
@@ -1,7 +0,0 @@
-colors=["Voilet", "Indigo", "Blue", "Green"]
-colors.reverse()
-print(colors)
-
-num=[4, 2, 5, 3, 6, 1, 2, 1, 2, 8, 9, 7]
-num.reverse()
-print(num)
---
+++ reverse_in_list.py
@@ -0,0 +1,19 @@
+# simple program to reverse the list
+
+# defining a list of elements
+colors=["Voilet", "Indigo", "Blue", "Green"]
+
+# reversing the list of elements
+colors.reverse()
+
+# printing the reversed list
+print(colors)
+
+# defining a list of numbers
+num=[4, 2, 5, 3, 6, 1, 2, 1, 2, 8, 9, 7]
+
+# reversing the list of numbers
+num.reverse()
+
+# printing the reversed list
+print(num)
