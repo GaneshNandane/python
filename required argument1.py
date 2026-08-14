8573b112f512caa2f549859ea9e1931856af20ad
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-14 22:37:04 +0530

    Update and rename required argument1.py to required_argument.py

--- required argument1.py
+++
@@ -1,3 +0,0 @@
-def name(fname, mname, lname):
-    print("Hello,", fname, lname)
-name("Peter", "Quill")
---
+++ required_argument.py
@@ -0,0 +1,12 @@
+# simple program to demonstrate the use of required arguments
+
+# defining a function with three arguments 
+# Note: in the function call we need to call all three arguments to run our program 
+
+def name(fname, mname, lname):
+    
+    # printing the arguments with greeting 
+    print("Hello,", fname, mname, lname)
+
+# calling a function
+name("Peter", "Ego", "Qull")
