190f69db99a67766c1f166547423542d597f9226
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-14 22:56:22 +0530

    Update and rename reverse sorting in list using sort.py to reverse_sorting_in_list_using_sort.py

--- reverse sorting in list using sort.py
+++
@@ -1,7 +0,0 @@
-colors=["Voilet", "Indigo", "Blue", "Green"]
-colors.sort(reverse=True)
-print(colors)
-
-num=[4, 2, 5, 3, 6, 1, 2, 1, 2, 8, 9, 7]
-num.sort(reverse=True)
-print(num)
---
+++ reverse_sorting_in_list_using_sort.py
@@ -0,0 +1,19 @@
+# simple program to sort the list in reversed order
+
+# defining the list of elements
+colors=["Voilet", "Indigo", "Blue", "Green"]
+
+# sorting the list of elements in reversed order from Z-to-A
+colors.sort(reverse=True)
+
+# printinig the reversed sorted list
+print(colors)
+
+# defining a list of numbers
+num=[4, 2, 5, 3, 6, 1, 2, 1, 2, 8, 9, 7]
+
+# sorting the list of numbers in reversed order from 9-to-1
+num.sort(reverse=True)
+
+# printing the reversed sorted list
+print(num)
