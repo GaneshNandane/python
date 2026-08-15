14d0571d5092a8e15342fae93184490bfe957036
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-15 21:51:49 +0530

    Update and rename set methodmain.py to checking_if_the_certain_value_is_persent_in_set.py

---
+++ checking_if_the_certain_value_is_persent_in_set.py
@@ -0,0 +1,10 @@
+# simple program to check if any certain value is present inside the set or not
+
+# defining a set
+info={"Carla", 19, False, 5.9}
+
+# checking if Carla is present inside info set or not using if else conditional block 
+if "Carla" in info:
+    print("Carla is present.")
+else:
+    print("Carla is absent.")    
