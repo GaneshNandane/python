0183213167e61ed119e2da79ba60f2f62237c372
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-15 22:04:50 +0530

    Update and rename setsmain.py to sets_main.py

---
+++ sets_main.py
@@ -0,0 +1,24 @@
+# simple program about sets printing traversing and different sets combinations
+
+# defining a set of only numbers
+s={2, 4, 2, 6}
+
+# printing numbers set to the console
+print(s)
+
+# printing the mixed set of numbers strings booleans float int values
+info = {"Corla", 19, False, 5.9, 19}
+
+# printing the mixed set to the console
+print(info)
+
+# creating the set
+harry = set()
+
+# checking the set type
+print(type(harry))
+
+# traversing the set values using for loop and printing each set value to the console 
+for value in info:
+    print(value)
+    
