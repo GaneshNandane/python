bc2db4a2d8e9697ef91b1e181d1df1399db13680
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-16 22:31:38 +0530

    Update and rename simple variable declaration .py to simple_variable_declaration.py

---
+++ simple_variable_declaration.py
@@ -0,0 +1,36 @@
+# simple program that demonstrate the declaration of variables
+
+# variable is a placeholder where we store some value value can be of different types
+  # types of variable
+    # int
+    # float
+    # boolean
+    # string
+
+# example for int
+# defining a variable
+number_1 = 5
+
+# printing the variable type
+print(type(number_1))
+
+# example for float
+# defining a variable
+number_2 = 5.3
+
+# printing the variable type
+print(type(number_2))
+
+# example for string
+# defining a variable
+string_variable = "Ganesh Nandane"
+
+# printing the variable type
+print(type(string_variable))
+
+# example for boolean
+# defining a variable
+boolean_variable = True
+
+# printing the variable type
+print(type(boolean_variable))
