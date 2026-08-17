d9a2f7ba86899b58297c46f5a4e0f8aaa9439049
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-17 22:20:33 +0530

    Update and rename slicing example.py to slicing_example.py

--- slicing example.py
+++
@@ -1,5 +0,0 @@
-pie="ApplePie"
-print(pie[:5])      #Slicing fron start
-print(pie[5:])      #Slicing till End
-print(pie[2:6])     #Slicing in between
-print(pie[-8:])     #Slicing using negative index
---
+++ slicing_example.py
@@ -0,0 +1,16 @@
+# simple program for string slicing
+
+# defining a string
+pie="ApplePie"
+
+# printing five index from strart
+print(pie[:5])      #Slicing fron start
+
+# printing rest of the index from 5th index
+print(pie[5:])      #Slicing till End
+
+# printing index between 2 to six
+print(pie[2:6])     #Slicing in between
+
+# printing using negative indexing 
+print(pie[-8:])     #Slicing using negative index
