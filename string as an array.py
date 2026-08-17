bd95dc9c8073cc1256f4367b89dbfe01568f77cc
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-17 22:38:41 +0530

    Update and rename string as an array.py to string_as_an_array.py

--- string as an array.py
+++
@@ -1,3 +0,0 @@
-pie="ApplePie"
-print(pie[:5])
-print(pie[6]) #retuns character at specified index
---
+++ string_as_an_array.py
@@ -0,0 +1,10 @@
+# Simple program to demonstrate string indexing and slicing
+
+# Define a string
+pie = "ApplePie"
+
+# Print the first five characters of the string using slicing
+print(pie[:5])
+
+# Print the character at index 6
+print(pie[6])  # Returns the character at the specified index
