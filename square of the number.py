024aa0b6d6c285d7b13c2745173567361c820f43
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-17 22:30:17 +0530

    Add files via upload

---
+++ square of the number.py
@@ -0,0 +1,4 @@
+a = int(input("Enter the number:"))
+print("The square of the number is:", a**2) # this is the standard use 
+print("The square of the number is:", a*a)
+# print("The square of the number is:", a^2) this is not valid because python does not use this symbol as the exponetation symbol instead it use it as the XOR operator
