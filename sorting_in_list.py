90a92d035942a0d62631a4d31502ab5eb1900e8c
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-17 22:29:23 +0530

    Enhance sorting_in_list.py with explanatory comments
    Added comments to explain sorting of colors and numbers.

---
+++ sorting_in_list.py
@@ -0,0 +1,20 @@
+# simple program to use sorting method
+
+# defining a list of elements
+colors=["Voilet", "Indigo", "Blue", "Green"]
+
+# applying sort method on the variable
+colors.sort()
+
+# printing sorted list
+# Note: it sorts in accending order
+print(colors)
+
+# defining a list of numbers
+num=[4, 2, 5, 3, 6, 1, 2, 1, 2, 8, 9, 7]
+
+# applying sort method on the number variable
+num.sort()
+
+# printing the sorted numbers list
+print(num)
