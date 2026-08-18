123b25779afd6cb35096533cacf0c9aef5ce9a5d
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-18 22:45:17 +0530

    Update and rename string methods find.py to string_methods_find.py

---
+++ string_methods_find.py
@@ -0,0 +1,8 @@
+# Simple program to demonstrate the use of the find() string method
+
+# Define a string
+str1 = "He's name is Dan. He is an honest man."
+
+# Find the position of the first occurrence of the substring "is"
+# The find() method returns the index of the first matching character
+print(str1.find("is"))
