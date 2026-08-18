1d6e9f860482497eee6acfc5353762ac8d1a96c9
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-18 22:13:46 +0530

    Update and rename string methods endswith2.py to string_methods_endswith.py

--- string methods endswith2.py
+++
@@ -1,2 +0,0 @@
-str1="Welcome to the Console!!!"
-print(str1.endswith("to",4,10))
---
+++ string_methods_endswith.py
@@ -0,0 +1,8 @@
+# Simple program to demonstrate the use of the endswith() string method
+
+# Define a string
+str1 = "Welcome to the Console!!!"
+
+# Check whether the substring "to" occurs at the end of the string
+# within the specified range from index 4 up to, but not including, index 10
+print(str1.endswith("to", 4, 10))
