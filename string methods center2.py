8d7a08347071b47d79acf99cc854f7ccbbf2f51b
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-18 21:49:07 +0530

    Enhance string_methods_center.py with comments
    Added comments to explain the use of the center() method.

--- string methods center2.py
+++
@@ -1,2 +0,0 @@
-str1="Welcome to the Consloe!!!"
-print(str1.center(50,"."))
---
+++ string_methods_center.py
@@ -0,0 +1,8 @@
+# Simple program to demonstrate the use of the center() string method
+
+# Define a string
+str1 = "Welcome to the Console!!!"
+
+# Center the string within a width of 50 characters
+# Fill the remaining space with dots (.)
+print(str1.center(50, "."))
