c3e2e3cedd20173680d12b2b171c6e2f7ee7f6de
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-18 21:25:35 +0530

    Update and rename string methods capitalize.py to string_methods_capitalize.py

---
+++ string_methods_capitalize.py
@@ -0,0 +1,22 @@
+# Simple program to demonstrate the use of the capitalize() method
+
+# Define a string with all lowercase letters
+str1 = "hello"
+
+# Capitalize the first character of the string
+# The capitalize() method converts the first character to uppercase
+# and converts the remaining characters to lowercase
+capstr1 = str1.capitalize()
+
+# Print the capitalized string
+print(capstr1)
+
+
+# Define a string containing both lowercase and uppercase letters
+str2 = "hello worlD"
+
+# Capitalize the first character and convert the remaining characters to lowercase
+capstr2 = str2.capitalize()
+
+# Print the capitalized string
+print(capstr2)
