885484cdfdb9e2d4f66b4ab03bbb9421d3d902c5
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-24 15:47:36 +0530

    Update and rename string methods isprintable.py to string_methods_isprintable.py

--- string methods isprintable.py
+++
@@ -1,2 +0,0 @@
-str1="We wish you a Merry christmas"
-print(str1.isprintable())
---
+++ string_methods_isprintable.py
@@ -0,0 +1,7 @@
+# simple program to check if the string are printable or not
+
+# defining a string
+str1="We wish you a Merry christmas"
+
+# checking if the string is printable or not using the isprintable method
+print(str1.isprintable())
