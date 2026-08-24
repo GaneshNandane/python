ccef8e47455900a3812144bbb83b0189d6441a7c
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-24 15:43:05 +0530

    Update and rename string methods islower.py to string_methods_islower.py

---
+++ string_methods_islower.py
@@ -0,0 +1,7 @@
+# simple program to check if all the text in a string are lowercase or not
+
+# defining a string
+str1="hello world"
+
+# checking if the string is in lowercase or not using islower method
+print(str1.islower())
