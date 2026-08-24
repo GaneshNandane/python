61a8c29f7083d5289a0fd5471e4d0d20f55b389f
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-24 15:54:23 +0530

    Refactor whitespace check program
    Updated comments and formatting for clarity.

--- string methods isspace.py
+++
@@ -1,4 +0,0 @@
-str1="  "
-print(str1.isspace())#using Spacebar
-str2="  "
-print(str2.isspace())#using Tab
---
+++ string_methods_isspace.py
@@ -0,0 +1,14 @@
+# Simple program to check whether all characters in a string are whitespace
+
+# Define a string containing one space
+str1 = " "
+
+# Check whether all characters in the string are whitespace
+print(str1.isspace())  # Using the Spacebar
+
+
+# Define a string containing two spaces
+str2 = "  "
+
+# Check whether all characters in the string are whitespace
+print(str2.isspace())  # Using the Spacebar
