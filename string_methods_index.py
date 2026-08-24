59c1900418547855919620d78bbd261767924529
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-24 15:23:55 +0530

    Update and rename string methods index.py to string_methods_index.py

---
+++ string_methods_index.py
@@ -0,0 +1,7 @@
+# Simple example of using the index() string method
+
+# Define a string
+str1 = "He's name is Dan. Dan is an honest man."
+
+# Find the index of the first occurrence of the word "Dan"
+print(str1.index("Dan"))
