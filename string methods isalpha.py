348372329f44bd118c1df57bb615b9bc8510a2fd
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-24 15:39:26 +0530

    Update and rename string methods isalpha.py to string_methods_isalpha.py

--- string methods isalpha.py
+++
@@ -1,2 +0,0 @@
-str1="Welcome"
-print(str1.isalpha())
---
+++ string_methods_isalpha.py
@@ -0,0 +1,7 @@
+# simple program to check if all the text are alphabetic characters or not
+
+# defining a string
+str1="Welcome"
+
+# checking if all the characters are alphabetic characters
+print(str1.isalpha())
