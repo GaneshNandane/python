7d8af916fe5612d764b96b4ef4ff3527e7eafdd4
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-24 15:32:49 +0530

    Update and rename string methods isalnum.py to string_methods_isalnum.py

---
+++ string_methods_isalnum.py
@@ -0,0 +1,8 @@
+# simple program to check if all the text is alphanumeric or not
+
+# defining a string
+str1="WelcomeToTheConsole"
+
+# checking if the text is alphanumeric or not 
+# Note: alphanumeric means text should contain letters and numbers 
+print(str1.isalnum())
