458bfb94328fde30efabc6036e894bbbd3cbe32f
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-25 12:36:49 +0530

    Update and rename string methods istitle.py to string_methods_istitle.py

--- string methods istitle.py
+++
@@ -1,2 +0,0 @@
-str1="World Health Organization"
-print(str1.istitle())
---
+++ string_methods_istitle.py
@@ -0,0 +1,7 @@
+# Simple program to check whether the first letter of each word is uppercase
+
+# Define a string
+str1 = "World Health Organization"
+
+# Check whether the first letter of each word is uppercase
+print(str1.istitle())
