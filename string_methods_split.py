020b45aee19119256488ea95150ed2ac44867d5d
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-25 12:50:45 +0530

    Update and rename string methods Split.py to string_methods_split.py

---
+++ string_methods_split.py
@@ -0,0 +1,7 @@
+# Simple program to demonstrate the use of the split() string method
+
+# Define a string
+str2 = "Silver Spoon"
+
+# Split the string into a list using the space as the separator
+print(str2.split(" "))
