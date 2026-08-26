246aeb24fc684480421363edee2adc4ec21455c0
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-26 22:01:06 +0530

    Enhance string method example with comments
    Added comments to explain the purpose of the code and updated the string content.

---
+++ string_methods_startswith.py
@@ -0,0 +1,7 @@
+# Simple program to demonstrate the use of the startswith() string method
+
+# Define a string
+str1 = "Python is an Interpreted Language"
+
+# Check whether the string starts with the specified text "Python"
+print(str1.startswith("Python"))
