b0fa723f1d2958edde4721d3e9a04aedb33240f5
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-26 22:12:43 +0530

    Update and rename string methods title.py to string_methods_title.py

---
+++ string_methods_title.py
@@ -0,0 +1,8 @@
+# Simple program to demonstrate the use of the title() string method
+
+# Define a string
+str1 = "He's name is Dan. Dan is an honest man."
+
+# Convert the first letter of each word to uppercase
+# and convert the remaining letters of each word to lowercase
+print(str1.title())
