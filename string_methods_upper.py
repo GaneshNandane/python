4270c1b1b824efd4f8602c3825dc1749f8786e3b
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-26 22:15:25 +0530

    Update and rename string methods upper.py to string_methods_upper.py

---
+++ string_methods_upper.py
@@ -0,0 +1,7 @@
+# Simple program to demonstrate the use of the upper() string method
+
+# Define a string
+str1 = "AbcDEfghI"
+
+# Convert all alphabetic characters in the string to uppercase
+print(str1.upper())
