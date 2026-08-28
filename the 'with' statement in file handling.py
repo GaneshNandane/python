c38861a81519fa25f734caecad9e14751deaf34c
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-28 23:08:32 +0530

    Update and rename the 'with' statement in file handling.py to the_'with'_statement_in_file_handling.py

--- the 'with' statement in file handling.py
+++
@@ -1,2 +0,0 @@
-with open('file for manipulating files.py', 'r') as f:
-    #...... do somthing with the file.
---
+++ the_'with'_statement_in_file_handling.py
@@ -0,0 +1,5 @@
+# simple program to know about with statement 
+# with is used to automatically open or close the file 
+
+with open('file for manipulating files.py', 'r') as f:
+    #...... do somthing with the file.
