e0fc5a84474b0356c5cecbd901e44febca8ad692
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-28 23:16:50 +0530

    Update and rename the dir method.py to the_dir_method.py

---
+++ the_dir_method.py
@@ -0,0 +1,8 @@
+# Simple program to demonstrate the use of the dir() function
+
+# Define a simple list
+x = [1, 2, 3]
+
+# Use the dir() function to display the attributes and methods
+# available for the list object
+print(dir(x))
