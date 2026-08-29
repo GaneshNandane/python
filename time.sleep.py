2eca0d50deab741214c2fe03ad6ce29879bc78a5
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-29 20:06:21 +0530

    Update time.sleep.py

--- time.sleep.py
+++ time.sleep.py
@@ -1,3 +1,6 @@
+# simple program to demonstrate the use of sleep function
+
+# here program sleeps for 2 seconds and again prints the output after 2 seconds
 import time
 print("Start:", time.time())
 time.sleep(2)
