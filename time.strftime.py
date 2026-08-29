8027be8044d524687a8819e9e13aedca4d4a7224
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-29 20:15:29 +0530

    Update time.strftime.py

--- time.strftime.py
+++ time.strftime.py
@@ -1,3 +1,6 @@
+# simple program to demonstrate the use strftime() function
+
+# here it converts it into string according to format you provide 
 import time
 t = time.localtime()
 formatted_time = time.strftime("%Y-%m-%d %H:%M:%S",t)
