9ec3c8a12170e3d17ff35e34644df5f9fec05ff5
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-29 19:23:03 +0530

    Revise if-else statement and output message
    Updated the if-else statement documentation and corrected the output for the else case.

--- the_use_of_single_code_in_string.py
+++ the_use_of_single_code_in_string.py
@@ -1,6 +1,7 @@
 ''' This is an if-else statement
 It will execute a block of code if a condition is true
 If the condition is false then it will execute another block of code.'''
+
 p=7
 if(p>5):
     print("P is greater than 5")
