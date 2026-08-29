83f860d7508a2f02ff058815ea1c244a010b7168
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-29 19:17:59 +0530

    Replace triple quotes with single line comments

---
+++ the_use_of_double_code_in_string.py
@@ -0,0 +1,9 @@
+# This is an if-else statement.It will execute a block of code if a specified condition is true.
+# If the condition is false then it will execute another block of code 
+
+p=7
+if(p>5):
+    print("P is greater than 5")
+else:
+    print("P is not greater than 5")
+  
