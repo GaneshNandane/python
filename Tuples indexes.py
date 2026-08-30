11314c4cb059f3d544d37a193a535d1bb723cae3
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-08-30 23:37:02 +0530

    Update and rename Tuples indexes.py to tuples_indexes.py

--- Tuples indexes.py
+++
@@ -1,3 +0,0 @@
-country=("Spain", "Italy", "India",)
-#         [0]       [1]     [2]
-print(country)
---
+++ tuples_indexes.py
@@ -0,0 +1,9 @@
+# simple program to demostrate how tuple indexes works 
+# Note: tuples also has the similar indexing as lists and it always starts with 0 
+
+# defining a tuple 
+country=("Spain", "Italy", "India",)
+#         [0]       [1]     [2]
+
+# printing the tuple 
+print(country)
