fe2c3c69af1d821f82acdf0a6c762a4eb809c6af
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-09-01 21:46:55 +0530

    Update and rename virtual environment main.py to virtual_environment_main.py

---
+++ virtual_environment_main.py
@@ -0,0 +1,17 @@
+# Simple program to demonstrate the use of a virtual environment
+
+# Create a virtual environment using the terminal:
+# python -m venv myenv
+
+# Activate the virtual environment:
+# Windows: myenv\Scripts\activate
+# Linux/macOS: source myenv/bin/activate
+
+# Install pandas inside the virtual environment:
+# pip install pandas
+
+# Import the pandas library
+import pandas as pd
+
+# Print the installed version of pandas
+print(pd.__version__)
