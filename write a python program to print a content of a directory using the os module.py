892e55991e78c6d4b16b97f7c314ac9d392f4074
Ganesh Nandane <nandaneganesh08@gmail.com>
2026-09-04 23:51:29 +0530

    Add files via upload

---
+++ write a python program to print a content of a directory using the os module.py
@@ -0,0 +1,10 @@
+import os
+
+# specify the directory you want
+directory_path = '/'
+
+# list all the files and directories in the specified path
+contnets = os.listdir(directory_path)
+
+# print the contents of the directoy
+print(contnets)  
